91083437708ee17fbdc93eba301bef8d763b2ae4
karabulutkenan <karabulutkenan@gmail.com>
2024-07-02 14:41:43 +0300

    Pettitions modeli eklendi

---
+++ EmekAkademisi/Migrations/20240702114122_AddPetitionSamples.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EmekAkademisi.Migrations
+{
+    public partial class AddPetitionSamples : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PetitionSamples",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(255)", maxLength: 255, nullable: false),
+                    FilePath = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    FileType = table.Column<string>(type: "nvarchar(10)", maxLength: 10, nullable: false),
+                    UploadDate = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PetitionSamples", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PetitionSamples");
+        }
+    }
+}
---
+++ EmekAkademisi/Models/PetitionSample.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace EmekAkademisi.Models
+{
+    public class PetitionSample
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string Title { get; set; } = "";
+
+        public string FilePath { get; set; } = "";
+
+        [Required]
+        [StringLength(10)]
+        public string FileType { get; set; } = "";
+
+        public DateTime UploadDate { get; set; } = DateTime.Now;
+    }
+}
